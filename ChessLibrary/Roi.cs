9edad2f66e432c5c2cb50560a95d38b09dc0749b
Unknown <kiwol95@gmail.com>
2019-01-30 11:31:21 +0100

    New abstract class

--- ChessLibrary/Roi.cs
+++ ChessLibrary/Roi.cs
@@ -10,7 +10,17 @@ namespace Fr.CodingFactory.ChessMate.Pieces
 
         public override bool IsValidMove(int xD, int yD, int xA, int yA)
         {
-            throw new NotImplementedException();
+            int xMove = xA - yA;
+            int yMove = yA - yD;
+
+            if((xMove > 0 && xMove <= 1 && yMove == 0) || (yMove > 0 && yMove <= 1 && xMove == 0) || (xMove < 0 && xMove >= -1 && yMove == 0) || (yMove < 0 && yMove >= -1 && xMove == 0))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }

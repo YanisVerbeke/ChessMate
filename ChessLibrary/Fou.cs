64536a8e2059d6f2638e9149b1ee6fa8b2d8de68
Pita-6 <pierre.crbs@gmail.com>
2019-01-30 11:30:53 +0100

    Fou

--- ChessLibrary/Fou.cs
+++ ChessLibrary/Fou.cs
@@ -3,6 +3,26 @@ namespace Fr.CodingFactory.ChessMate.Pieces
 {
     public class Fou : Piece
     {
+        public override bool IsValidMove(int xD, int yD, int xA, int yA)
+        {
+            int xMove = xA - xD;
+            int yMove = yA - yD;
+
+            if ((xMove > 0 && xMove <= 7 && yMove > 0 && yMove <= 7) || (xMove > 0 && xMove >= 7 && yMove > 0 && yMove >= 7) || (xMove > 0 && xMove <= 7 && yMove > 0 && yMove >= 7) || (xMove > 0 && xMove >= 7 && yMove > 0 && yMove <= 7))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public override bool IsTrajectoireLibre(int xD, int yD, int xA, int yA, object[][] Piece)
+        {
+            throw new NotImplementedException();
+        }
 
     }
 }

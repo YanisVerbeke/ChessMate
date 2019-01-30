769b3b1d72abf236731fdc56fb94afebe39f202b
Unknown <yanis88357@gmail.com>
2019-01-30 11:28:24 +0100

    Auto stash before merge of "master" and "origin/master"

--- ChessLibrary/Fou.cs
+++ ChessLibrary/Fou.cs
@@ -3,6 +3,14 @@ namespace Fr.CodingFactory.ChessMate.Pieces
 {
     public class Fou : Piece
     {
+        public override bool IsTrajectoireLibre(int xD, int yD, int xA, int yA, object[][] Piece)
+        {
+            throw new NotImplementedException();
+        }
 
+        public override bool IsValidMove(int xD, int yD, int xA, int yA)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- ChessLibrary/Roi.cs
+++ ChessLibrary/Roi.cs
@@ -3,6 +3,14 @@ namespace Fr.CodingFactory.ChessMate.Pieces
 {
     public class Roi : Piece
     {
-        
+        public override bool IsTrajectoireLibre(int xD, int yD, int xA, int yA, object[][] Piece)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsValidMove(int xD, int yD, int xA, int yA)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
